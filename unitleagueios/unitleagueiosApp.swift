6e0a7a1b0404a5cc331a8f7e74046a580e450f84
Quinn Fargen <quinnfargen@gmail.com>
2025-11-07 22:01:47 -0600

    Initial Commit

---
+++ unitleagueios/unitleagueiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitleagueiosApp.swift
+//  unitleagueios
+//
+//  Created by Quinn Fargen on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct unitleagueiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
