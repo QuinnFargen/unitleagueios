bf4bb35bbfae5821c7393804484fe4580be1b623
Quinn Fargen <quinnfargen@gmail.com>
2026-05-17 15:20:00 -0400

    api unitleague apiclient

--- unitleagueios/Services/APIClient.swift
+++ unitleagueios/Services/APIClient.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 enum APIClient {
-    static let baseURL = "http://192.168.4.59:8000"
+//    static let baseURL = "http://192.168.4.59:8000"
+    static let baseURL = "https://api.unitleague.com"    
 }
